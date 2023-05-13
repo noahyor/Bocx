3ae320db36c6d5834a94775f7eb23cf49b2430ae
noahyor <noah.yorgey@gmail.com>
2023-05-13 18:34:40 +0000

    Wall Object

---
+++ Wall.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Wall:
+
+    def __init__(self, startPos, endPos):
+        self.start = startPos
+        self.end = endPos
+
+    def update(self):
+        pass
+    
+    def draw(self, surface):
+        pygame.draw.line(surface, "black", self.start, self.end, width=3)
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+# TODO: Use sprites instead of manually rendering
+
 import pygame, sys
-from Player import *
+from Player import Player
+from Wall import Wall
 from pygame.locals import QUIT
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Bocx')
 clock = pygame.time.Clock()
 
-things = [Player()]
+things = [Player(), Wall((0, 250),(250, 200))]
 
 def update():
     for thing in things:
         thing.update()
 
 def draw():
-    DISPLAYSURF.fill("black")
+    DISPLAYSURF.fill("grey")
     for thing in things:
         thing.draw(DISPLAYSURF)
-  
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
